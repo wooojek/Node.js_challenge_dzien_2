e418c941dc971bf0765a2be04209be882aa3045d
wooojek <yokomura123@gmail.com>
2018-01-31 22:04:08 +0100

    second day assignment 1 finished

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,18 @@
-//Twój kod
+const fs = require('fs');
+
+fs.readFile('./data/zadanie01/input.json', (err, data) => {
+    if (err !== null) {
+        console.log('Error reading the file');
+    } else {
+        const parsedData = JSON.parse(data);
+        const dataSum = parsedData.reduce((prev, curr) => prev + curr);
+        console.log('Success loading file');
+        fs.writeFile('./data/zadanie01/input.txt', dataSum, err => {
+            if (err !== null) {
+                console.log('Error writing to file');
+            } else {
+                console.log('File write successful');
+            }
+        })
+    }
+});
