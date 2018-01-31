c363da5f78d898e456bc3749e7f99e73e675f14e
wooojek <yokomura123@gmail.com>
2018-01-31 22:15:29 +0100

    second day assignment 2 finished

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,19 @@
-//Twój kod
+const fs = require('fs');
+
+const path = './data/zadanie02';
+
+fs.readdir(path, (err, files) => {
+    if (err !== null) {
+        console.log('There was problem listing files');
+    } else {
+        files.forEach(file => {
+            fs.readFile(path + `/${file}`, (err, data) => {
+               if (err !== null) {
+                   console.log(`There was problem reading ${file} file`);
+               } else {
+                   console.log(`${file}: ${data}`);
+               }
+            });
+        })
+    }
+});
