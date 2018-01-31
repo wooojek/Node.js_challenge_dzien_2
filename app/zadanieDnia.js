74c6b75117d49b6d79b7dab85a973b61c50682f9
wooojek <yokomura123@gmail.com>
2018-01-31 22:42:57 +0100

    second day assignment 3 finished

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,19 @@
-//Twój kod
+const fs = require('fs');
+
+const fileName = process.argv[2];
+
+fs.readFile(fileName, 'utf-8', (err, data) => {
+   if (err !== null) {
+       console.log('There was an error reading the file.');
+   } else {
+       console.log('File read success');
+       let modifiedText = data.split('').map((sign, index) => index % 2 === 0 ? sign.toUpperCase() : sign.toLowerCase()).join('');
+       fs.writeFile(fileName, modifiedText, err => {
+           if (err !== null) {
+               console.log(`There was problem writing a file: ${fileName}`);
+           } else {
+               console.log('File modified');
+           }
+       });
+   }
+});
